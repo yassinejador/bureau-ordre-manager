850e121f317f49d213b4cc2c05c3b3f4b7ebda30
Yassine Jador <yassinejador@gmail.com>
2025-02-08 00:38:46 +0000

    added dbtables.sql, data.sql and picture of MLD

---
+++ data.sql
@@ -0,0 +1,109 @@
+use db_bureau_ordre_manager;
+
+-- Insertion des rôles
+INSERT INTO roles (role, date_creation) VALUES
+('Direction', CURDATE()),
+('Secrétariat général', CURDATE()),
+('Responsables de départements', CURDATE()),
+('Agents administratifs', CURDATE());
+
+-- Insertion des permissions
+INSERT INTO permissions (nom) VALUES 
+('Accéder au tableau de bord'),
+('Créer un nouveau courrier arriver'),
+('Créer un nouveau courrier depart'),
+('Supprimer un courrier'),
+('Archiver un courrier'),
+('Générer des rapports mensuels et annuels'),
+('Créer un role'),
+('Gérer les permissions'),
+('Consulter la liste des utilisateurs');
+
+
+-- Insertion des établissements
+INSERT INTO etablissements (intitule, adresse, ville, fax, telephone) VALUES
+('Établissement A', '123 Rue A', 'VilleA', 123456789, 987654321),
+('Établissement B', '456 Rue B', 'VilleB', 223456789, 887654321);
+
+-- Insertion des services
+INSERT INTO Services (nom, date_creation) VALUES
+('Service Courrier', CURDATE()),
+('Service Informatique', CURDATE());
+
+-- Insertion des utilisateurs
+INSERT INTO users (email, password, nom, prenom, role_id, etablissement_id, service_id, date_creation) VALUES
+('admin@example.com', 'password123', 'Doe', 'John', 1, 1, 1, CURDATE()),
+('user@example.com', 'password123', 'Smith', 'Alice', 2, 2, 2, CURDATE());
+
+-- Insertion des départements
+INSERT INTO departements (intitule, responsable_id) VALUES
+('Département A', 1),
+('Département B', 2);
+
+-- Insertion des états
+INSERT INTO etats (etat) VALUES
+('Enattente'),
+('Traite'),
+('Cloture');
+
+-- Insertion des courriers
+INSERT INTO courriers (expediteur, destination, traite_par, objet, etat_id, date_creation) VALUES
+(1, 2, 1, 'Demande de renseignements', 1, now()),
+(2, 1, 2, 'Réponse à la demande', 2, now());
+
+-- Insertion des fichiers
+INSERT INTO fichiers (type_courrier, type_support, fichier, courrier_id) VALUES
+('confidential', 'numerique', 'file1.pdf', 1),
+('urgent', 'papier', 'file2.pdf', 2);
+
+-- Insertion des logs
+INSERT INTO Logs (user_id, description, date_action) VALUES
+(1, 'Création de courrier', CURDATE()),
+(2, 'Modification de courrier', CURDATE());
+
+-- Insertion des permissions détaillées
+INSERT INTO permissionDetails (role_id, permission_id, hasPermission) VALUES
+-- Rôle 1 :
+(1, 1, TRUE),
+(1, 2, TRUE),
+(1, 3, TRUE),
+(1, 4, TRUE),
+(1, 5, TRUE),
+(1, 6, TRUE),
+(1, 7, TRUE),
+(1, 8, TRUE),
+(1, 9, TRUE),
+
+-- Rôle 2 :
+(2, 1, TRUE),
+(2, 2, TRUE),
+(2, 3, TRUE),
+(2, 4, TRUE),
+(2, 5, TRUE),
+(2, 6, TRUE),
+(2, 7, TRUE),
+(2, 8, TRUE),
+(2, 9, TRUE),
+
+-- Rôle 3 :
+(3, 1, TRUE),
+(3, 2, TRUE),
+(3, 3, TRUE),
+(3, 4, TRUE),
+(3, 5, TRUE),
+(3, 6, TRUE),
+(3, 7, TRUE),
+(3, 8, TRUE),
+(3, 9, TRUE),
+
+-- Rôle 4 :
+(4, 1, TRUE),
+(4, 2, TRUE),
+(4, 3, TRUE),
+(4, 4, TRUE),
+(4, 5, TRUE),
+(4, 6, TRUE),
+(4, 7, TRUE),
+(4, 8, TRUE),
+(4, 9, TRUE);
+
---
+++ dbtables.sql
@@ -0,0 +1,99 @@
+create DATABASE db_bureau_ordre_manager;
+
+use db_bureau_ordre_manager;
+
+CREATE TABLE `users`(
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `nom` VARCHAR(255) NOT NULL,
+    `prenom` VARCHAR(255) NOT NULL,
+    `role_id` INT NOT NULL,
+    `etablissement_id` INT NOT NULL,
+    `service_id` INT NOT NULL,
+    `date_creation` DATE NOT NULL
+);
+CREATE TABLE `departements`(
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `intitule` VARCHAR(255) NOT NULL,
+    `responsable_id` INT NOT NULL
+);
+CREATE TABLE `fichiers`(
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `type_courrier` ENUM('confidential', 'urgent') NOT NULL,
+    `type_support` ENUM('papier', 'numerique') NOT NULL,
+    `fichier` VARCHAR(255) NOT NULL,
+    `courrier_id` INT NOT NULL
+);
+CREATE TABLE `etablissements`(
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `intitule` VARCHAR(255) NOT NULL,
+    `adresse` VARCHAR(255) NOT NULL,
+    `ville` VARCHAR(255) NOT NULL,
+    `fax` BIGINT NOT NULL,
+    `telephone` BIGINT NOT NULL
+);
+CREATE TABLE `courriers`(
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `expediteur` INT NOT NULL,
+    `destination` INT NOT NULL,
+    `traite_par` INT NOT NULL,
+    `objet` TEXT NOT NULL,
+    `etat_id` INT NOT NULL,
+    `date_creation` DATE NOT NULL,
+    `date_suppression` DATETIME NULL
+);
+CREATE TABLE `etats`(
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `etat` ENUM('Enattente', 'Traite', 'Cloture') NOT NULL
+);
+CREATE TABLE `roles`(
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `role` VARCHAR(255) NOT NULL,
+    `date_creation` DATE NOT NULL
+);
+CREATE TABLE `permissions`(
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `nom` VARCHAR(255) NOT NULL
+);
+CREATE TABLE `Services`(
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `nom` VARCHAR(255) NOT NULL,
+    `date_creation` DATE NOT NULL
+);
+CREATE TABLE `Logs`(
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `user_id` INT NOT NULL,
+    `description` TEXT NOT NULL,
+    `date_action` DATE NOT NULL
+);
+CREATE TABLE `permissionDetails`(
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `role_id` INT NOT NULL,
+    `permission_id` INT NOT NULL,
+    `hasPermission` BOOLEAN NOT NULL
+);
+ALTER TABLE
+    `permissionDetails` ADD CONSTRAINT `permissiondetails_role_id_foreign` FOREIGN KEY(`role_id`) REFERENCES `roles`(`id`);
+ALTER TABLE
+    `users` ADD CONSTRAINT `users_service_id_foreign` FOREIGN KEY(`service_id`) REFERENCES `Services`(`id`);
+ALTER TABLE
+    `users` ADD CONSTRAINT `users_role_id_foreign` FOREIGN KEY(`role_id`) REFERENCES `roles`(`id`);
+ALTER TABLE
+    `fichiers` ADD CONSTRAINT `fichiers_courrier_id_foreign` FOREIGN KEY(`courrier_id`) REFERENCES `courriers`(`id`);
+ALTER TABLE
+    `Logs` ADD CONSTRAINT `logs_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `users`(`id`);
+ALTER TABLE
+    `courriers` ADD CONSTRAINT `courriers_traite_par_foreign` FOREIGN KEY(`traite_par`) REFERENCES `users`(`id`);
+ALTER TABLE
+    `courriers` ADD CONSTRAINT `courriers_etat_id_foreign` FOREIGN KEY(`etat_id`) REFERENCES `etats`(`id`);
+ALTER TABLE
+    `departements` ADD CONSTRAINT `departements_responsable_id_foreign` FOREIGN KEY(`responsable_id`) REFERENCES `users`(`id`);
+ALTER TABLE
+    `courriers` ADD CONSTRAINT `courriers_destination_foreign` FOREIGN KEY(`destination`) REFERENCES `etablissements`(`id`);
+ALTER TABLE
+    `permissionDetails` ADD CONSTRAINT `permissiondetails_permission_id_foreign` FOREIGN KEY(`permission_id`) REFERENCES `permissions`(`id`);
+ALTER TABLE
+    `courriers` ADD CONSTRAINT `courriers_expediteur_foreign` FOREIGN KEY(`expediteur`) REFERENCES `etablissements`(`id`);
+ALTER TABLE
+    `users` ADD CONSTRAINT `users_etablissement_id_foreign` FOREIGN KEY(`etablissement_id`) REFERENCES `etablissements`(`id`);
