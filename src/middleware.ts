56d04ef74a96b40dd57ef519f1718cbf93a0235b
Taghlaoui Oussama <oussamataghlaoui20@gmail.com>
2025-02-20 14:14:33 +0100

    Add user role fetching and enhance middleware for authentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@
  * This program is strictly temporary and intended solely for testing purposes.
  * It is completely insecure.
  */
-
+/*
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
@@ -56,4 +56,62 @@ export const config = {
     "/logs/:path*",
     "/services/:path*",
   ],
+};
+*/
+
+
+import jwt from "jsonwebtoken";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const token = req.cookies.get("auth_token")?.value;
+  const path = req.nextUrl.pathname;
+
+  let decoded: any = null;
+  if (token) {
+    decoded = jwt.decode(token); 
+  }
+
+  if (decoded) {
+    try {
+      const userId = decoded.id;
+      const userResponse = await fetch(`http://localhost:3000/api/users/${userId}`);
+      
+      if (!userResponse.ok) {
+        const errorText = await userResponse.text(); 
+        console.error("Erreur lors de la récupération de l'utilisateur :", errorText);
+        return NextResponse.json(
+          { error: "Erreur serveur ou utilisateur non trouvé" },
+          { status: 500 }
+        );
+      }
+      const user = await userResponse.json();
+      console.log(user);
+
+    } catch (error) {
+      console.error("Erreur lors de la récupération de l'utilisateur :", error);
+      return NextResponse.json(
+        { error: "Erreur serveur lors de la récupération de l'utilisateur" },
+        { status: 500 }
+      );
+    }
+  }
+
+  if (token && path === "/login") {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  if (path === "/login") {
+    return NextResponse.next();
+  }
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|icon.png|images/logo.*).*)"], 
 };
